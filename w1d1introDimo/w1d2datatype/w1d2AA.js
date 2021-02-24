bd4137eca627ce2b0dbdfeb25197cbd33bcf07fa
temesgen <temesgensego@gmail.com>
2021-02-24 10:52:57 -0500

    Create w1d2AA.js

---
+++ w1d1introDimo/w1d2datatype/w1d2AA.js
@@ -0,0 +1,67 @@
+
+// Q1A1
+// //taking volume input from the user
+// Convert cubic centimeters to liters. 
+// 1liter=1,000cm^{{3}}
+// const  Volume= prompt("Enter value in volume: ")
+
+// // conversion factor
+// const factor = 1,000cm^{{3}}
+
+// // calculate liters
+// const liters= (volume/4)* 1/factor
+
+// console.log(`${volume/4} volume is equal to ${liters} miles.`);
+
+// Q2A 
+// //taking kilometers input from the user
+// const kilometers = prompt("Enter value in kilometers: ")
+
+// // conversion factor
+// const factor = 0.621371
+
+// // calculate miles
+// const miles = kilometers * factor
+
+// console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
+
+
+// 1. What will be the output of running following code?
+// console.log(Number(null));
+// console.log(Number(undefined));
+// console.log(Boolean("0"));
+// console.log(Boolean(" "))
+// answer
+// 0
+// NaN
+// true
+// true
+// 2. What will be the output of running following code?
+// let a = b = 2;
+// let c = 3 - (a + b + 1);
+// console.log(a--);
+// console.log(++c);
+// console.log(a);
+// console.log(c);
+// answer 
+// 2
+// -1
+// 1
+// -1
+
+// 3. What is the significance of + operator before prompt function in following code
+// let a = +prompt("First number?");
+// repesenting parsint 
+// 4. What is the use of || and && operators (logical-or and logical-and) in JavaScript? Explain with an
+// example of each. 
+
+// The logical OR ( || ) operator (logical disjunction) for a set of operands is true if and only if one or more of its operands is true.
+//  It is typically used with Boolean (logical) values. 
+// When it is, it returns a Boolean value
+// The logical AND ( && ) operator (logical conjunction) for a set of operands is true if and only if all of its operands are true. 
+// It is typically used with Boolean (logical) values. When it is, it returns a Boolean value.
+
+// 5. Which of the following statements is true?
+// a. In computer programming, a statement can be part of an expression. answer true
+// b. In computer programming, an expression can be a part of a statement. answer true
+
